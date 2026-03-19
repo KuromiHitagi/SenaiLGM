48183a3cb9350ac48ab3dc4fff0db3e22a88620a
Nicolas Freitas Souza <darktails076@gmail.com>
2026-03-19 11:55:44 -0300

    Atualização da pizzaria, do css e from.js

--- Pizzaria/js/footer.js
+++ Pizzaria/js/footer.js
@@ -1,15 +1,20 @@
 const footer = document.getElementById("footer");
 
 footer.innerHTML = `
-                    <div class="email">
+                    <section class="footerSection">
 
+                        <div class="email">
+
+                            <span class="mailSpan">E-Mail: <a href="mailTo:forno&codigo@gmail.com">forno&codigo@gmail.com</a></span>
                         
-                    
-                    </div>
-                    
-                    <div class="telefone">
-                    
+                        </div>
+                        
+                        <div class="telefone">
+                        
+                            <span class="telSpan">Telefone: <a>+55 (11) 4002-8922</a></span>
+                        
+                        </div>
 
+                    </section>
                     
-                    </div>
                     `;
