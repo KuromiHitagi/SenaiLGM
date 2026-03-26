b76deb84a389691a92f2d2791f36159436c0ddfe
Nicolas Freitas Souza <darktails076@gmail.com>
2026-03-26 12:01:59 -0300

    Diversas atualizações, todos os arquivos, CSS's, Footer.js, Index's

--- Aulas/footer.js
+++ Aulas/footer.js
@@ -1,6 +1,6 @@
 const footer = document.querySelector('footer');
 footer.innerHTML = `
-    <div>
+    <div id="contatoDiv">
         <h4>Entre em Contato</h4>
         <p>
             Email: <a href="mailto:darktails076@gmail.com">darktails076@gmail.com</a>
@@ -16,5 +16,34 @@ footer.innerHTML = `
         <p>
             Made By: <br>Nicolas Freitas <br>16/02/2026
         </p>
+        <button onclick="Modo()" id="btn-Modo">Modo <span id="modo"></span></button>
     </div>
 `;
+
+const btnModo = document.getElementById("btn-Modo")
+const modo = document.getElementById("modo")
+let contador = 0;
+
+function verify() {
+    if(contador === 0) {
+        modo.innerText = "Escuro"
+    } else if(contador === 1) {
+        modo.innerText = "Claro"
+    }
+}
+
+setInterval(() => {
+    verify();
+}, 500)
+
+function Modo() {
+    if(contador === 0) {
+        footer.classList.remove("black")
+        footer.classList.add("white")
+    } else if(contador === 1) {
+        footer.classList.remove("white")
+        footer.classList.add("black")
+    }
+
+    contador = (contador === 0) ? 1 : 0;
+}
