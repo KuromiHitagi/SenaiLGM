a938fa932e0aa5bb1c7432b7b6c468486ae34b29
Nicolas Freitas Souza <darktails076@gmail.com>
2026-06-10 11:54:59 -0300

    Correção do formulário e restrição

--- Pizzaria/js/form.js
+++ Pizzaria/js/form.js
@@ -1,15 +1,11 @@
-function FormularioCliente1() {
+function FormularioCliente() {
     const form = document.getElementById("form")
 
     form.innerHTML = `
                     <div class="title">
-                        <h2>Cadastro de Usuário: </h2>
+                        <h2>Cadastre-se</h2>
                     </div>
 
-                    <label class="Ccod" for="Ccod">Código</label>
-                    <input class="Ccod" id="Ccod" type="text" size="10" value="1" readonly>
-
-                    <br>
 
                     <div class="dados"> 
                         <div class="labelInput">
@@ -18,7 +14,7 @@ function FormularioCliente1() {
                             <input id="Cnome" type="text" maxLength="45" size="50" >
                         </div>
                         
-                        
+
 
                         <div class="labelInput">
                             <p>Digite seu email:</p>
@@ -37,12 +33,27 @@ function FormularioCliente1() {
                         
                         
                         <div class="labelInput">
-                            <p>Confirme a senha enviada:</p>
-                            <label for="CconfirmPassword">Confirmar Senha:</label>
-                            <input id="CconfirmPassword" type="password" maxLength="45" size="50" >
+                            <p>Informe seu endereço:</p>
+                            <label for="Cendereco">Endereço:</label>
+                            <input id="Cendereco" type="text" maxLength="45" size="50" >
                         </div>
 
-                        <br>
+
+
+                        <div class="labelInput">
+                            <p>Coloque seu CPF sem pontos e traço:</p>
+                            <label for="Ccpf">CPF:</label>
+                            <input id="Ccpf" type="text" maxLength="11" size="50" >
+                        </div>
+
+
+
+                        <div class="labelInput">
+                            <p>Insira seu Telefone sem traço:</p>
+                            <label for="Ctelefone">Telefone:</label>
+                            <input id="Ctelefone" type="text" maxLength="11" size="50" >
+                        </div>
+                        
                     </div>
 
                     <div class="Btns">
@@ -60,57 +71,4 @@ function FormularioCliente1() {
                     </div>
                     `
 }
-FormularioCliente1();
-
-function FormularioCliente2() {
-    const form = document.getElementById("form")
-
-    form.innerHTML = `
-                    <div class="title">
-                        <h2>Concluir Cadastro: </h2>
-                    </div>
-            
-                    <label class="Ccod" for="Ccod">Código</label>
-                    <input class="Ccod" id="Ccod" type="text" size="10" value="1" readonly> 
-
-                    <div class="dados">
-                        <div class="labelInput">
-                            <p>Informe seu endereço para as entregas</p>
-                            <label for="Cendereco">Endereço:</label>
-                            <input id="Cendereco" type="text" maxLength="45" size="50" required>
-                        </div>
-
-                        <br>
-
-                        <div class="labelInput">
-                            <p>Digite seu telefone para mantermos contato</p>
-                            <label for="Ctelefone">Telefone</label>
-                            <input id="Cendereco" type="text" maxlength="45" size="50" required>
-                        </div>
-
-                        <br>
-
-                        <div class="labelInput">
-                            <p>Escreva seu CPF</p>
-                            <label for="Ccpf">CPF</label>
-                            <input id="Ccpf" type="text" maxlength="45" size="50" required>
-                        </div>
-                    </div>
-                    
-                    <br>
-
-                    <div class="Btns">
-                        
-                        <div class="btnDiv C">
-                            <p>Volta a página e apaga os dados</p>
-                            <button class="Btn-Cancel" onclick="Cancelar()" type="reset">Cancelar</button>
-                        </div>
-                        
-                        <div class="btnDiv A">
-                            <p>Conclui o seu cadastro</p>
-                            <button class="Btn-Approve" onclick="Prosseguir()" type="button">Concluir</button>
-                        </div>
-                    
-                    </div>
-                    `
-}
+FormularioCliente();
